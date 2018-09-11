4dfacb5aa9d14116d47b5be0e2fa5f745f37138e
KatyaKalache <katya@kalache.fr>
2018-09-11 18:46:27 +0000

    Fixed stule according to semistandard and addign Readme

--- holbertonschool-webstack_basics/0x02-javascript_basics/0-multi_languages.js
+++ holbertonschool-webstack_basics/0x02-javascript_basics/0-multi_languages.js
@@ -1,5 +1,5 @@
 #!/usr/bin/node
 // Prints 3 sentences
-console.log("C is fun");
-console.log("Python is cool");
-console.log("Javascript is amazing");
+console.log('C is fun');
+console.log('Python is cool');
+console.log('Javascript is amazing');
