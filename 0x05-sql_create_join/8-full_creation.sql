c6f9879fc858608f662f6f2e4f7042ce17d208d9
KatyaKalache <katya@kalache.fr>
2018-09-17 20:31:04 +0000

    removing name of column in values

--- 0x05-sql_create_join/8-full_creation.sql
+++ 0x05-sql_create_join/8-full_creation.sql
@@ -1,6 +1,6 @@
 -- creates second table with several rows
 CREATE TABLE IF NOT EXISTS second_table(id INT, name VARCHAR(256), score INT);
-INSERT INTO second_table(id, name, score) VALUE(id = 1, name = 'John', score = 10);
-INSERT INTO second_table(id, name, score) VALUE(id = 2, name = 'Alex', score = 3);
-INSERT INTO second_table(id, name, score) VALUE(id = 3, name = 'Bob', score = 14);
-INSERT INTO second_table(id, name, score) VALUE(id = 4, name = 'George', score = 8);
+INSERT INTO second_table(id, name, score) VALUES(1, 'John', 10);
+INSERT INTO second_table(id, name, score) VALUES(2, 'Alex', 3);
+INSERT INTO second_table(id, name, score) VALUES(3, 'Bob', 14);
+INSERT INTO second_table(id, name, score) VALUES(4, 'George', 8);
