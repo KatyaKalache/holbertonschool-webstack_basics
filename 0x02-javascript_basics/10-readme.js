0a5cbc92744fc71406a74e444f2708042af52220
KatyaKalache <katya@kalache.fr>
2018-10-01 18:57:50 +0000

    return error

--- 0x02-javascript_basics/10-readme.js
+++ 0x02-javascript_basics/10-readme.js
@@ -3,8 +3,7 @@
 var fs = require('fs');
 fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
-    console.log(err);
+    return console.log(err);
   }
   console.log(data);
-}
-);
+});
