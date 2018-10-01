4307219d6ba8f0b44ec24ac5e2d896f64c346c61
KatyaKalache <katya@kalache.fr>
2018-10-01 18:53:16 +0000

    taking in argument as file name

--- 0x02-javascript_basics/10-readme.js
+++ 0x02-javascript_basics/10-readme.js
@@ -1,7 +1,7 @@
 #!/usr/bin/node
 // reads and prints the content of a file
 var fs = require('fs');
-fs.readFile('cisfun', 'utf8', function (err, data) {
+fs.readFile(process.argv[2], 'utf8', function (err, data) {
   if (err) {
     console.log(err);
   }
